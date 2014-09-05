3ec07a0e7a792a96a86eaea052edb7fc02188467
Max Bernardy <mbernardy@appnexus.com>
2014-09-05 12:56:15 -0400

    Update icons to use bootstrap and match open/close state for folders

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,45 @@
 $(function() {
-
+	var open_icon = 'glyphicon glyphicon-folder-open';
+	var close_icon = 'glyphicon glyphicon-folder-close';
 
 	$('#root').jstree({
 		core : {
 			data : {
 				url : "tree",
 				data : function(node){
-					console.log("Getting path for node ", node);
 					var path = this.get_path(node, "/", true);
-					console.log("Got path ", path );
 					if (node.id  != "#"){
-						console.log("Returning data ", path);
 						return { path : path};
 					}
+				},
+				dataFilter : function(data, type){
+					var results = JSON.parse(data);
+					results.forEach(function(node){
+						if (!node.children){
+							node.type = 'file';
+						}
+					})
+					return JSON.stringify(results);
 				}
 			}
+		},
+		types: {
+			file : {
+				icon : 'glyphicon glyphicon-file',
+				valid_children : []
+			},
+			'default' : {
+				icon : close_icon
+			}
+		},
+		plugins : ['types']
+	}).on('before_open.jstree after_close.jstree', function(e, data){
+		var node = data.node;
+		if(node.state && node.state.opened){
+			data.instance.set_icon(node, open_icon);
+		}else{
+			data.instance.set_icon(node, close_icon);
 		}
-	});
+	})
 
 });
